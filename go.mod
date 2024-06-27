bebef0ddec2ee886d0e9d7f5b0591cd97f17f578
joaoavilars <joao.avila.rs@gmail.com>
2024-06-27 16:41:57 -0300

    incluido listagem de grupos e contatos e arquivo de conf

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module sendWhats.go
 
 go 1.21.1
+
+require gopkg.in/ini.v1 v1.67.0 // indirect
--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
+
+	"gopkg.in/ini.v1"
 )
 
 type Message struct {
@@ -14,11 +18,28 @@ type Message struct {
 	Message string `json:"message"`
 }
 
-func main() {
-	url := "http://192.168.10.39:4000/api/message"
-	phone := os.Args[1]
-	title := os.Args[2]
-	message := strings.Join(os.Args[3:], " ")
+type Group struct {
+	ID   string `json:"id"`
+	Name string `json:"name,omitempty"`
+}
+
+var serverURL string
+
+func loadConfig() {
+	cfg, err := ini.Load("sendwhats.conf")
+	if err != nil {
+		fmt.Println("Erro ao carregar arquivo de configuração:", err)
+		os.Exit(1)
+	}
+	serverURL = cfg.Section("").Key("site").String()
+	if serverURL == "" {
+		fmt.Println("Endereço do servidor não especificado no arquivo de configuração.")
+		os.Exit(1)
+	}
+}
+
+func sendMessage(phone, title, message string) {
+	url := serverURL + "/api/message"
 
 	// Substitui \n por quebras de linha reais
 	message = strings.ReplaceAll(message, "\\n", "\n")
@@ -54,4 +75,63 @@ func main() {
 
 	// Verifica o status da resposta
 	fmt.Println("Status da resposta:", resp.Status)
+}
+
+func getGroups() {
+	url := serverURL + "/api/groups"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Erro ao fazer a requisição:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Erro ao ler a resposta:", err)
+		return
+	}
+
+	var groups []Group
+	err = json.Unmarshal(body, &groups)
+	if err != nil {
+		fmt.Println("Erro ao decodificar resposta JSON:", err)
+		return
+	}
+
+	fmt.Println("Lista de Grupos:")
+	for _, group := range groups {
+		if group.Name != "" {
+			fmt.Printf("ID: %s - Nome: %s\n", group.ID, group.Name)
+		} else {
+			fmt.Printf("ID: %s - Nome: (sem nome)\n", group.ID)
+		}
+	}
+}
+
+func main() {
+	// Carrega a configuração
+	loadConfig()
+
+	// Define a flag para listar grupos
+	listGroups := flag.Bool("groups", false, "Listar grupos")
+	flag.Parse()
+
+	if *listGroups {
+		getGroups()
+		return
+	}
+
+	// Parâmetros para enviar mensagem
+	if len(os.Args) < 4 {
+		fmt.Println("Uso: ./meuapp <phone> <title> <message>")
+		return
+	}
+
+	phone := os.Args[1]
+	title := os.Args[2]
+	message := strings.Join(os.Args[3:], " ")
+
+	sendMessage(phone, title, message)
 }
